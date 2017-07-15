77c62cd0bacabed18297df52aba74215e9b79e90
Aman Mahajan <ammahajan@DELC02TMC24G8WN.sea.corp.expecn.com>
2017-07-16 00:30:02 +0530

    REST Tweet API working

--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ import json
 from urlparse import urlparse,parse_qs
 import urllib
 from requests_oauthlib import OAuth1
+import webbrowser
 
 app = Flask(__name__)
 apiKey = "P9HvIxlPA2luSxEzdq0g5iznR"
@@ -25,13 +26,33 @@ def makeRequest(OAuthToken,OAuthVerifier):
 	
 	print(response.content)
 
+	token = str(parse_qs(urlparse('?'+str(response.content)).query)['oauth_token'])
+	secret = str(parse_qs(urlparse('?'+str(response.content)).query)['oauth_token_secret'])
+	userId = str(parse_qs(urlparse('?'+str(response.content)).query)['user_id'])
+	name = str(parse_qs(urlparse('?'+str(response.content)).query)['screen_name'])
+
+	oauth = OAuth1(apiKey,
+                   client_secret=apiSecret,
+                   resource_owner_key=token[2:-2],
+                   resource_owner_secret=secret[2:-2]
+                   )
+
+	tweetsUrl = "https://api.twitter.com/1.1/account/verify_credentials.json"
+
+	response = requests.get(url=tweetsUrl,auth=oauth)
+
+	return response.content
+
+
+
 @app.route('/')
 def data():
 	try:
 	    OAuthToken = request.args.get('oauth_token')
 	    OAuthVerifier = request.args.get('oauth_verifier')
-	    makeRequest(OAuthToken,OAuthVerifier)
-	    return json.dumps({"oauthtoken":OAuthToken,"oauthverifier":OAuthVerifier})    
+	    x = makeRequest(OAuthToken,OAuthVerifier)
+	    return json.dumps({"tweets":x})
+	    #return json.dumps({"oauthtoken":OAuthToken,"oauthverifier":OAuthVerifier})    
 
 	except:
 		return json.dumps({"response":"false"})
@@ -50,6 +71,29 @@ def make():
 	OAuthToken = str(parse_qs(urlparse('?'+str(response.text)).query)['oauth_token'])
 	OAuthSecret = OAuthSecret[2:-2]
 	OAuthToken = OAuthToken[2:-2]
+
+	# MacOS
+	try:
+		chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+		webbrowser.get(chrome_path).open("https://api.twitter.com/oauth/authenticate?oauth_token=" + str(OAuthToken))
+	except:
+		print "You don't use mac!"
+
+	# Windows
+	try:
+		chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
+		webbrowser.get(chrome_path).open("https://api.twitter.com/oauth/authenticate?oauth_token=" + str(OAuthToken))
+
+	except:
+		print "You don't use windows!"
+	
+	# Linux
+	try:
+		chrome_path = '/usr/bin/google-chrome %s'
+		webbrowser.get(chrome_path).open("https://api.twitter.com/oauth/authenticate?oauth_token=" + str(OAuthToken))
+	except:
+		print "You don't use linux!"
+
 	return json.dumps({"oauthtoken":OAuthToken,"oauthsecret":OAuthSecret}) 
 
 
